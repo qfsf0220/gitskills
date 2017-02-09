4ed4b701ec2581840e45f6801b16fbc4b7604396
qfsf0220 <371204418@qq.com>
2017-02-09 15:47:38 +0800

    YP commit

--- test170207/test170207.py
+++ test170207/test170207.py
@@ -1,10 +1,21 @@
-a = [x for x in range(10) if x%2==0]
+a=lambda x,y :zip(x,y)
 
+abc = iter(a([1,2,66,7,8,11,345,4,23,234,44],[3,5,55,6,9,23,4,54,345,3]))
 
+def outzip(n):
+    b = 1
+    for i in range(10000):
+        if b==n:
+            try:
+                print(next(abc))
+            except StopIteration:
+                break
+        else:
+            try:
+                next(abc)
+            except StopIteration:
+                break
+        b+=1
 
-b=map(lambda x:x,range(10))
-for i in a:
-    print(i,sep='~~',end=' ')
-
-for i in b:
-    print(i)
+if __name__ == '__main__':
+    outzip(8)
