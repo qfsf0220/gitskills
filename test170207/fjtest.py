b4908d0fee82feb0afba47dd0923823c0130afaa
qfsf0220 <371204418@qq.com>
2017-02-13 18:36:27 +0800

    YP commit

--- test170207/fjtest.py
+++ test170207/fjtest.py
@@ -7,7 +7,7 @@ from pyquery import PyQuery as pq
 import re
 
 b=[]
-a=['徐汇臻园','东湾度假村','苑宏新村','中海瀛台','徐汇苑','南方新村','盛华景苑','徐汇华园','紫阳花园','印象欧洲','浦润苑','华唐苑','银泰苑','新凯家园一期','中冶锦城']
+a=['徐汇臻园','东湾度假村','苑宏新村','中海瀛台','徐汇苑','南方新村','盛华景苑','徐汇华园','紫阳花园','印象欧洲','浦润苑','华唐苑','银泰苑','新凯家园一期','中冶锦城','漓江山水花园','华泾绿苑','华泾五村','华欣家园','东湾小区','馨宁公寓','徐汇新干线','爱庐世纪新苑','新弘国际城','南方城','晶欣坊','华发小区','枫桦景苑','华建一街坊','华沁家园','长桥一村','长桥五村']
 ab=['中海瀛台','徐汇臻园','徐汇华园']
 for i in a:
     url='http://shanghai.anjuke.com/sale/rd1?from=zjsr&kw=%s' % i #安居客
@@ -16,29 +16,23 @@ for i in a:
     opener = urllib.request.build_opener()
     opener.addheaders = [headers]
     data = opener.open(url2).read()
-    #url3='http://www.dooioo.com/ershoufang/rs%s' % i #链家
+
     url3='http://sh.lianjia.com/ershoufang/rs%s' % i #链家
     url4=urllib.parse.quote(url3,safe=string.printable)
 
-    #webpage=urllib.request.urlopen(url2)
-    #data=webpage.read()
     data=data.decode('utf-8')
-    #print(data)
+
     webpage2=urllib.request.urlopen(url4)
     data2=webpage2.read()
-    #data2=data.decode('utf-8')
 
     d=pq(data)
     d2=pq(data2)
-   #print(d('span').text()+'|'+d('em').filter('.price').text()+'|'+d('em').filter('.up').text())
-
 
     b.append([d('.comm-price').text(),d2('.wrapper').find('.botline').eq(0).text()])
 def greenprint(ssttrr):
     return ('\033[0;32;40m%s\033[0m' % ssttrr)
 def redprint(ssttrr):
     return ('\033[0;31;40m%s\033[0m' % ssttrr)
-
 def yellowprint(ssttrr):
     return ('\033[0;33;40m%s\033[0m' % ssttrr)
 
@@ -46,16 +40,10 @@ c=sorted(b,key=lambda x:re.sub('\D','',x[0]))
 
 d= map(lambda x:re.sub('( （(.*)\))','',x[0]),c)
 e= [x for x  in d ]
-# for i in c:
-#     if '↑' in i[0].split(' ')[5]:
-#         print(i[0].split(' ')[0] + " 本月" + yellowprint(i[0].split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + redprint((i[0].split(' ')[5])))
-#     else:
-#         print(i[0].split(' ')[0] + " 本月" + yellowprint(i[0].split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint((i[0].split(' ')[5])))
+
 for i in e:
     if '↑' in i.split(' ')[5]:
         print(i.split(' ')[0]+" 本月" + yellowprint(i.split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " +  redprint(i.split(' ')[5])   )
     else:
         print(i.split(' ')[0] + " 本月" + yellowprint(i.split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint(
             i.split(' ')[5]))
-
-
