5e637022c7823d850aa72c9caa94f700a1d6d94c
qfsf0220 <371204418@qq.com>
2017-02-13 18:28:36 +0800

    YP commit

--- test170207/fjtest.py
+++ test170207/fjtest.py
@@ -8,7 +8,7 @@ import re
 
 b=[]
 a=['徐汇臻园','东湾度假村','苑宏新村','中海瀛台','徐汇苑','南方新村','盛华景苑','徐汇华园','紫阳花园','印象欧洲','浦润苑','华唐苑','银泰苑','新凯家园一期','中冶锦城']
-ab=['南方新村','华泾五村','徐汇华园']
+ab=['中海瀛台','徐汇臻园','徐汇华园']
 for i in a:
     url='http://shanghai.anjuke.com/sale/rd1?from=zjsr&kw=%s' % i #安居客
     url2=urllib.parse.quote(url,safe=string.printable)
@@ -44,12 +44,18 @@ def yellowprint(ssttrr):
 
 c=sorted(b,key=lambda x:re.sub('\D','',x[0]))
 
-for i in c:
-    if '↑' in i[0].split(' ')[5]:
-        print(i[0].split(' ')[0] + " 本月" + yellowprint(i[0].split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + redprint((i[0].split(' ')[5])))
+d= map(lambda x:re.sub('( （(.*)\))','',x[0]),c)
+e= [x for x  in d ]
+# for i in c:
+#     if '↑' in i[0].split(' ')[5]:
+#         print(i[0].split(' ')[0] + " 本月" + yellowprint(i[0].split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + redprint((i[0].split(' ')[5])))
+#     else:
+#         print(i[0].split(' ')[0] + " 本月" + yellowprint(i[0].split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint((i[0].split(' ')[5])))
+for i in e:
+    if '↑' in i.split(' ')[5]:
+        print(i.split(' ')[0]+" 本月" + yellowprint(i.split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " +  redprint(i.split(' ')[5])   )
     else:
-        print(i[0].split(' ')[0] + " 本月" + yellowprint(i[0].split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint(
-            (i[0].split(' ')[5])))
+        print(i.split(' ')[0] + " 本月" + yellowprint(i.split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint(
+            i.split(' ')[5]))
 
-# for i in c[0]:
-#     print(c[1][0].split(' ')[0]+" 本月"+yellowprint(c[1][0].split(' ')[2].split('元')[0])+"元/平方米 , 比上月 "+redprint((c[0][0].split(' ')[-1])))
+
