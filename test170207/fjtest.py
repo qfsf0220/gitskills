f72143678be1f00c9be0d12b433af7f7129a9e4f
qfsf0220 <371204418@qq.com>
2017-02-14 17:00:28 +0800

    YP commit

--- test170207/fjtest.py
+++ test170207/fjtest.py
@@ -7,8 +7,8 @@ from pyquery import PyQuery as pq
 import re
 
 b=[]
-a=['徐汇臻园','东湾度假村','苑宏新村','中海瀛台','徐汇苑','南方新村','盛华景苑','徐汇华园','紫阳花园','印象欧洲','浦润苑','华唐苑','银泰苑','新凯家园一期','中冶锦城','漓江山水花园','华泾绿苑','华泾五村','华欣家园','东湾小区','馨宁公寓','徐汇新干线','爱庐世纪新苑','新弘国际城','南方城','晶欣坊','华发小区','枫桦景苑','华建一街坊','华沁家园','长桥一村','长桥五村']
-ab=['中海瀛台','徐汇臻园','徐汇华园']
+ab=['徐汇臻园','东湾度假村','苑宏新村','中海瀛台','徐汇苑','南方新村','盛华景苑','徐汇华园','紫阳花园','印象欧洲','浦润苑','华唐苑','银泰苑','新凯家园一期','中冶锦城','漓江山水花园','华泾绿苑','华泾五村','华欣家园','东湾小区','馨宁公寓','徐汇新干线','爱庐世纪新苑','新弘国际城','南方城','晶欣坊','华发小区','枫桦景苑','华建一街坊','华沁家园','长桥一村','长桥五村']
+a=['中海瀛台','徐汇臻园','徐汇华园','枫桦景苑']
 for i in a:
     url='http://shanghai.anjuke.com/sale/rd1?from=zjsr&kw=%s' % i #安居客
     url2=urllib.parse.quote(url,safe=string.printable)
@@ -37,13 +37,14 @@ def yellowprint(ssttrr):
     return ('\033[0;33;40m%s\033[0m' % ssttrr)
 
 c=sorted(b,key=lambda x:re.sub('\D','',x[0]))
+print (c)
+d= map(lambda x:re.sub('(（(.*)\))','',x[0]),c)
 
-d= map(lambda x:re.sub('( （(.*)\))','',x[0]),c)
 e= [x for x  in d ]
+print(e)
 
-for i in e:
-    if '↑' in i.split(' ')[5]:
-        print(i.split(' ')[0]+" 本月" + yellowprint(i.split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " +  redprint(i.split(' ')[5])   )
+for i in e :
+    if '↑' in re.split('\s+',i)[5]:
+        print(re.split('\s+',i)[0]+ " 本月"+yellowprint(re.split('\s+',i)[2].split('元')[0])+"元/平方米 , 比上月 " +redprint(re.split('\s+',i)[5]))
     else:
-        print(i.split(' ')[0] + " 本月" + yellowprint(i.split(' ')[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint(
-            i.split(' ')[5]))
+        print(re.split('\s+', i)[0] + " 本月" + yellowprint(re.split('\s+', i)[2].split('元')[0]) + "元/平方米 , 比上月 " + greenprint(re.split('\s+', i)[5]))
