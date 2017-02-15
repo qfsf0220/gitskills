ff089a68d3bfed2672801e5a8c79a2652a487d7f
qfsf0220 <371204418@qq.com>
2017-02-15 15:34:42 +0800

    YP commit

--- test170207/multitest.py
+++ test170207/multitest.py
@@ -12,12 +12,30 @@ from multiprocessing.pool import ThreadPool
 #     for i in str(range(10)):
 #         p=Process(target=hihi,args=(i,))
 #         p.start()
-def hi(i):
-    a=("hi", i)
-    return (a)
-if __name__ == '__main__':
-    for i in range(4):
+from  threading import Thread
+import time
 
-        p=Process(target=hi,args=(i,))
-        print(p)
-        p.start()
+def fooo(aa):
+    time.sleep(10)
+    return aa
+
+class Mythread(Thread):
+    def __init__(self,number):
+        Thread.__init__(self)
+        self.number = number
+
+    def run(self):
+        self.result = fooo(self.number)
+
+    def get_result(self):
+        return self.result
+
+t1=Mythread(3)
+t2=Mythread(5)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+print(t1.get_result())
+print(t2.get_result())
