81da2c04c5257838313d828053b8c06c29fbcffd
qfsf0220 <371204418@qq.com>
2017-02-21 14:54:46 +0800

    YP commit

--- testfile/testfile0221.py
+++ testfile/testfile0221.py
@@ -16,7 +16,39 @@ class test2(object):
         self.a = age
 
     def printino(self):
-        print("id:{},name:{},age:{}".format(self.n,self.i,self.a))
+        print("id:{},name:{},age:{}".format(self.n, self.i, self.a))
 
-b=test2("qf",1,29)
+b = test2("qf", 1, 29)
 b.printino()
+
+#重写
+class A:
+    def hello(self):
+        print("AAAAA")
+
+class B(A):
+    pass
+
+class C(A):
+    def hello2(self):
+        print("ccccc")
+b=C()
+b.hello()
+b.hello2()
+
+#属性
+class area:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+    #
+    # def setsize(self,size):
+    #     self.height,self.width=size
+
+    def getsize(self):
+        return self.height*self.width
+
+area = area()
+area.width=10
+area.height=20
+print(area.getsize())
