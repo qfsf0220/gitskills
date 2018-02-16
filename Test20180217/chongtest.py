7c64d249a2c82a4ec68f74b6d226b5be1c2c94b5
qf <371204418@qq.com>
2018-02-17 02:02:54 +0800

    pic->str

--- Test20180217/chongtest.py
+++ Test20180217/chongtest.py
@@ -1,15 +1,25 @@
 import builtwith
 
 # builtwith.parse("https://try.taobao.com")
-print(  builtwith.parse("http://idea.lanyus.com") )
+print(  builtwith.parse("http://www.taobao.com") )
 
 
 
 import whois as wi
 
 
-# print( whois.do_parse("http://idea.lanyus.com"))
+# print( whois.do_parse("http://www.taobao.com"))
 
 
-w=wi.whois("sohu.com")
+w=wi.whois("www.taobao.com")
 print(w)
+
+import urllib.robotparser as ur
+from urllib.robotparser import RobotFileParser as urf2
+
+# urf = ur.RobotFileParser()
+# urf2.set_url("http://qq.win/robot.txt")
+# urf2.read()
+
+
+
