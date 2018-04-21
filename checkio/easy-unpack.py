9b40e9120cccab874ca59b059f4ee44f9109fdb4
qfsf <371204418@qq.com>
2018-04-22 01:48:49 +0800

    commit xh

---
+++ checkio/easy-unpack.py
@@ -0,0 +1,10 @@
+def easy_unpack(elements):
+    a=[x for x in elements]
+    return(tuple([a[0],a[2],a[-2]]))
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)) == (1, 3, 7)
+    assert easy_unpack((1, 1, 1, 1)) == (1, 1, 1)
+    assert easy_unpack((6, 3, 7)) == (6, 7, 3)
+    print('Done! Go Check!')
---
+++ checkio/the-most-frequent.py
@@ -0,0 +1,22 @@
+from collections import  Counter
+def most_frequent(data):
+    a=Counter(data).most_common(1)
+    return(a[0][0])
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    print('Example:')
+    print(most_frequent([
+        'a', 'b', 'c', 
+        'a', 'b',
+        'a'
+    ]))
+    
+    assert most_frequent([
+        'a', 'b', 'c', 
+        'a', 'b',
+        'a'
+    ]) == 'a'
+
+    assert most_frequent(['a', 'a', 'bi', 'bi', 'bi']) == 'bi'
+    print('Done')
