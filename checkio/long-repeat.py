7a3b0a59934628ff1dbf03f1b5b7bc45f72fdb3c
qfsf <371204418@qq.com>
2018-04-21 11:53:54 +0800

    commit  wdeskin7

--- checkio/long-repeat.py
+++ checkio/long-repeat.py
@@ -1,9 +1,26 @@
+from  collections import Counter
 def long_repeat(line):
-    """
-        length the longest substring that consists of the same char
-    """
-    # your code here
-    return 0
+    # result=(Counter(line).most_common(1)[0][1]) if line!='' else 0
+    # print(result)
+    # return result
+    if  line != "":
+        aa=[]
+        parten = line[0]
+        count=1
+        for i in line[1:]:
+            if i == parten:
+                count+=1
+            else:
+                aa.append(parten+str(count))
+                parten=i
+                count=1
+        aa.append(parten+str(count))
+        print(aa)
+        print(sorted(aa,key=lambda x:x[1],reverse=True)[0][1])
+        return int(sorted(aa,key=lambda x:x[1],reverse=True)[0][1])
+    else :
+        return 0
+
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
@@ -12,3 +29,4 @@ if __name__ == '__main__':
     assert long_repeat('abababaab') == 2, "Third"
     assert long_repeat('') == 0, "Empty"
     print('"Run" is good. How is "Check"?')
+
