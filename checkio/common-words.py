86b9bf37d133e2c0a6d0f0e742459fe9eca6e53f
qf <371204418@qq.com>
2018-04-26 10:21:16 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Test20171020/test123.py

--- checkio/common-words.py
+++ checkio/common-words.py
@@ -1,8 +1,19 @@
 def checkio(first, second):
-    return ""
+    firstlist = first.split(',')
+    secondlist = second.split(',')
+    alist = sorted  (list(set(firstlist).intersection(set(secondlist)))  )
+
+    reslut =(",".join(alist)) if alist!=[] else ""
+    return  reslut
+
+
+checkio("hello,world", "hello,earth")
+checkio("one,two,three", "four,five,one,two,six,three")
+checkio("one,two,three", "four,five,six")
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
-if __name__ == '__main__':
-    assert checkio("hello,world", "hello,earth") == "hello", "Hello"
-    assert checkio("one,two,three", "four,five,six") == "", "Too different"
-    assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
+# if __name__ == '__main__':
+#     assert checkio("hello,world", "hello,earth") == "hello", "Hello"
+#     assert checkio("one,two,three", "four,five,six") == "", "Too different"
+#     assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
