88284cc64974dc1975eb2f9c1e079e104c5865e8
qfsf <371204418@qq.com>
2018-05-01 09:09:46 +0800

    commit  wdeskin7

---
+++ checkio/right-to-left.py
@@ -0,0 +1,15 @@
+from functools import  reduce
+def left_join(phrases):
+    alist = [x for x  in phrases]
+    for i in range(len(alist)) :
+        if "right" in alist[i]:
+            alist[i]=alist[i].replace("right","left")
+    return reduce(lambda x,y:x+","+y,alist)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop", "All to left"
+    assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
+    assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
+    assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
